7cf8cedcf4fca2efb010f433f0de687ad0a0f630
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-11-05 01:05:07 +0100

    updated api fetchmethod

--- src/services/getProduct.ts
+++ src/services/getProduct.ts
@@ -1,11 +1,19 @@
-import { IGetProductsResponse } from "../models";
+import axios from "axios";
+// import { IGetProductsResponse } from "../models";
 
 export const getProducts = async () => {
-  let response: IGetProductsResponse;
+  // let response: IGetProductsResponse;
 
-  response = require("../json/products.json");
+  // // response = require("../json/products.json");
+  // response = await axios.get(
+  //   "https://nkwo-data-default-rtdb.firebaseio.com/products.json"
+  // );
 
-  const { products } = response.data || [];
+  // const { products } = response.data || [];
 
-  return products;
+  // return products;
+
+  return await axios.get(
+    "https://nkwo-data-default-rtdb.firebaseio.com/products.json"
+  );
 };
